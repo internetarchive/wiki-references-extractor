dd0381b6bad13a761dee58cc952ea3b810a5caf6
James Hare <jamesmhare@gmail.com>
2024-09-11 05:18:47 +0000

    case insensitivity for section headers; detecting bare-link references

--- article.py
+++ article.py
@@ -1,20 +1,31 @@
 import mwparserfromhell
+import re
 import sys
 from list import extract_list_items
 from wikiapi import get_current_timestamp, get_wikipedia_article
 
+def extract_urls_from_text(text):
+    url_regex = re.compile(r'(?:git|https?|ftps?)://[^\s\]\|\}]+')
+    result = set(url_regex.findall(text))
+    return result
+
 def extract_references(wikitext):
     wikicode = mwparserfromhell.parse(wikitext)
-    reference_sections = ["Further reading", "External links", "Bibliography"]
+    reference_sections = ["further reading", "external links", "bibliography"]
     references = []
+    found_urls = set()
 
     # Extract <ref> tags content
     for tag in wikicode.filter_tags(matches=lambda node: node.tag == "ref"):
+        for url in extract_urls_from_text(str(tag)):
+            found_urls.add(url)
         references.append(str(tag))
 
     # Extract all {{Sfn}} templates in the body of the article
     for template in wikicode.filter_templates():
         if template.name.matches("Sfn"):
+            for url in extract_urls_from_text(str(template)):
+                found_urls.add(url)
             references.append(str(template))
 
     # Extract lines from specific sections
@@ -22,10 +33,18 @@ def extract_references(wikitext):
         section_title = section.filter_headings()
         if section_title:
             title_text = section_title[0].title.strip_code().strip()
-            if title_text in reference_sections:
+            if title_text.lower() in reference_sections:
                 for line in extract_list_items(section):
                     if line.strip().startswith('*') or line.strip().startswith('#'):
+                        for url in extract_urls_from_text(str(line)):
+                            found_urls.add(url)
                         references.append(line.strip())
+
+    # Extract external link nodes not attached to any other reference type
+    for external_link in wikicode.filter_external_links():
+        if str(external_link.url) not in found_urls:
+            references.append(str(external_link))
+
     return references
 
 def extract_references_from_page(title, domain="en.wikipedia.org", as_of=None):
